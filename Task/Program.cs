416069e4858c4aa7757284bf120b2cc88f091d55
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2023-03-14 23:25:19 +0300

    added task and solution

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Написать программу, которая из имеющегося массивастрок формирует массив из строк, длина которых меньше либо равна 3 символа. 
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
 
@@ -25,9 +25,23 @@ void PrintArray(string[] enteredArray)
     Console.WriteLine();
 }
 
-
+string [] NewArray (string [] result)
+{
+    string[] newArray = new string[result.Length];
+    int j = 0;
+    for (int i = 0; i < result.Length; i++)
+    {
+        if (result[i].Length <= 3)
+        {
+            newArray[j] = result[i];
+            j++;
+        }
+    }
+    return newArray;
+} 
 
 string [] result = InitArray ();
 PrintArray(result);
-
+string [] finalArray = NewArray (result);
+PrintArray(finalArray);
 
