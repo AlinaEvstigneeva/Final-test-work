e726dd91789edf313fdde4aa73aa697d7e231623
Alina Evstigneeva <alina_khalilova_95@mail.ru>
2023-03-14 11:49:12 +0300

    added array creation method

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,4 +2,32 @@
 Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
 
-/* Привет, пес */
+string [] InitArray ()
+{
+    Console.Write("Введите количество элементов массива:\t");
+    int count = int.Parse(Console.ReadLine());
+    
+    string[] enteredArray = new string[count];
+    for (int i = 0; i < enteredArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива по индексом {i}:\t ");
+        enteredArray[i] = Console.ReadLine();
+    }
+    return enteredArray;
+}
+
+void PrintArray(string[] enteredArray)
+{
+    for (int i = 0; i < enteredArray.Length; i++)
+    {
+        Console.Write($"{enteredArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+string [] result = InitArray ();
+PrintArray(result);
+
+
